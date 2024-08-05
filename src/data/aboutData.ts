fd9b0df43ca47b8cd2b457846b0809651cceff3e
SeedONova <78652589+SeedONova@users.noreply.github.com>
2024-08-05 15:36:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/data/aboutData.ts
@@ -0,0 +1,67 @@
+import AboutType from "@/types/About";
+
+export const AboutData: AboutType[] = [
+  {
+    logo: "/assets/careercompass-logo.png",
+    title: "CareerCompass",
+    description:
+      "Career mentoring platform, targeting 1st and 2nd year students that’s still exploring their passion, interests, and fields.",
+    igLink: "https://www.google.com/",
+    tiktokLink: "https://www.google.com/",
+    bgColor: "#C95454",
+  },
+  {
+    logo: "/assets/techfusion-logo.png",
+    title: "TechFusion",
+    description:
+      "Helping Indonesian IT talents explore their passion through programs, competitions, and community.",
+    igLink: "https://www.google.com/",
+    tiktokLink: "https://www.google.com/",
+    bgColor: "#9854C9",
+  },
+  {
+    logo: "/assets/comp-logo.png",
+    title: "Competition Realms",
+    description:
+      "One-stop competition hub for Indonesian students, promoting inclusivity and self-growth through competitive events.",
+    igLink: "https://www.google.com/",
+    tiktokLink: "https://www.google.com/",
+    bgColor: "#717660",
+  },
+  {
+    logo: "/assets/ltw-logo.png",
+    title: "LinkToWork",
+    description:
+      "Connecting top companies with motivated students through job fairs, CV reviews, and career prep programs.",
+    igLink: "https://www.google.com/",
+    tiktokLink: "https://www.google.com/",
+    bgColor: "#137CF8",
+  },
+  {
+    logo: "/assets/sisu-logo.png",
+    title: "SisuLab",
+    description:
+      "Community for bookworms and journaling enthusiasts, sharing excitement, wisdom, and self-improvement together.",
+    igLink: "https://www.google.com/",
+    tiktokLink: "https://www.google.com/",
+    bgColor: "#989898",
+  },
+  {
+    logo: "/assets/virtualxplore-logo.png",
+    title: "VirtualXplore",
+    description:
+      "A project-based career exploration platform that offers hands-on experience from Indonesia's top-leading companies.",
+    igLink: "https://www.google.com/",
+    tiktokLink: "https://www.google.com/",
+    bgColor: "#F8B200",
+  },
+  {
+    logo: "/assets/college-copilot-logo.png",
+    title: "College Copilot",
+    description:
+      "AI media and community initiated to co-navigate students harnessing AI in daily life through workshops and webinars.",
+    igLink: "https://www.google.com/",
+    tiktokLink: "https://www.google.com/",
+    bgColor: "#702FF9",
+  },
+];
---
+++ src/data/companyData.ts
@@ -0,0 +1,36 @@
+export const logos = [
+  { src: "/assets/logo/company-partners/Card.png", alt: "Indosat Ooredoo" },
+  { src: "/assets/logo/company-partners/Card-1.png", alt: "Unilever" },
+  { src: "/assets/logo/company-partners/Card-2.png", alt: "Bank BRI" },
+  { src: "/assets/logo/company-partners/Card-4.png", alt: "ULA App" },
+  {
+    src: "/assets/logo/company-partners/Card-5.png",
+    alt: "Astra International",
+  },
+  { src: "/assets/logo/company-partners/Card-6.png", alt: "Lazada" },
+  { src: "/assets/logo/company-partners/Card-7.png", alt: "Shopee" },
+  {
+    src: "/assets/logo/company-partners/Card-8.png",
+    alt: "Paragon Technology and Innovation",
+  },
+  { src: "/assets/logo/company-partners/Card-9.png", alt: "Deloitte" },
+];
+
+export const logos2 = [
+  { src: "/assets/logo/company-partners/Card-10.png", alt: "TikTok" },
+  { src: "/assets/logo/company-partners/Card-11.png", alt: "Narasi" },
+  { src: "/assets/logo/company-partners/Card-12.png", alt: "Danone" },
+  { src: "/assets/logo/company-partners/Card-13.png", alt: "Loreal" },
+  {
+    src: "/assets/logo/company-partners/Card-14.png",
+    alt: "PT HM Sampoerna Tbk.",
+  },
+  { src: "/assets/logo/company-partners/Card-15.png", alt: "Bank OCBC" },
+  {
+    src: "/assets/logo/company-partners/Card-16.png",
+    alt: "Schneider Electric",
+  },
+  { src: "/assets/logo/company-partners/Card-17.png", alt: "Mantappu Corp" },
+  { src: "/assets/logo/company-partners/Card-18.png", alt: "NutriFood" },
+  { src: "/assets/logo/company-partners/Card-19.png", alt: "P&G" },
+];
